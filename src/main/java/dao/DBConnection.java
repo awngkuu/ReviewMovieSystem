89ddda865d182c7e4ab21dabdb2542e99bcad32b
tuf <tuf@LAPTOP-KC8J1ULL>
2025-06-18 23:57:10 +0800

    Initial Commit

--- src/main/java/dao/DBConnection.java
+++ src/main/java/dao/DBConnection.java
@@ -13,14 +13,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-//    private static final String URL = "jdbc:mysql://localhost:3306/movie_review_system";
-//    private static final String USER = "root";
-//    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/movie_review_system";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
     
     
-    private static final String URL = System.getenv("DB_URL");
-    private static final String USER = System.getenv("DB_USER");
-    private static final String PASSWORD = System.getenv("DB_PASS");
+//    private static final String URL = System.getenv("DB_URL");
+//    private static final String USER = System.getenv("DB_USER");
+//    private static final String PASSWORD = System.getenv("DB_PASS");
     
 
     public static Connection getConnection() throws SQLException {
